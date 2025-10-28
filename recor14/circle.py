070f361a7e8ffc8b2f47a594a59bfd7d0d7b9b41
faaiquanayyira <faaiquanayyira@gmail.com>
2025-10-28 12:45:14 +0530

    completed

---
+++ recor14/circle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def circlearea(r):
+    print("Area Of Circle:",3.14*r*r)
+    
+def circleperi(r):
+    print("Perimeter Of Circle:",2*3.14*r)
+
+
+# In[2]:
+
+
+get_ipython().system('jupyter nbconvert --to python circle.ipynb')
+
+
+# In[ ]:
+
+
+
+
---
+++ recor14/rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def rectarea(l,b):
+    print("Area Of Rectangle:",l*b)
+    
+def rectperi(l,b):
+    print("Perimeter Of Rectangle:",2*(l+b))
+
+
+# In[4]:
+
+
+get_ipython().system('jupyter nbconvert --to python circle.ipynb')
+
+
+# In[ ]:
+
+
+
+
